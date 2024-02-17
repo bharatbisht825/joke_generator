99f6e9fa01709b628afc3d7fd9a4be6ac31c2a7d
bharatbisht825 <bharatbisht825@gmail.com>
2024-02-17 11:24:54 +0530

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+const jokeContainer=document.getElementById("joke")
+const btn=document.getElementById("but")
+const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+
+let getJoke=()=>{
+    fetch(url).then(data=>data.json()).then(item => {jokeContainer.textContent=`${item.joke}`;
+    });
+}
+btn.addEventListener("click",getJoke);
